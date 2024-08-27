d05ac748836399eacbbcd87443b5461bb447cdc6
hyw520110 <hyw520110@163.com>
2024-08-27 08:07:36 +0000

    Create startup.sh

---
+++ ollama/startup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 设置环境变量
+export OLLAMA_HOST=0.0.0.0:11434
+
+# 检查 tmux 是否已安装
+if ! command -v tmux &> /dev/null
+then
+    if [ -x "$(command -v apt)" ]; then
+        sudo apt-get update && sudo apt-get install -y tmux
+    elif [ -x "$(command -v yum)" ]; then
+        sudo yum install -y tmux
+    else
+        echo "请手动安装tmux。"
+        exit 1
+    fi
+fi
+
+if [ $(ps -ef | grep '[o]llama serve' | wc -l) -eq 0 ]; then
+  nohup ollama serve > ollama.log 2>&1 &
+  #ollama pull nomic-embed-text
+  ollama list
+fi
+
+if [ $(ps -ef | grep '[o]llama run qwen2:7b' | grep -v grep | wc -l) -eq 0 ]; then
+  #screen -dmS qwen2 -t xterm bash -c 'ollama run qwen2:7b; exec bash'
+  tmux new-session -d -s qwen2 'bash -c "ollama run qwen2:7b; exec bash"'
+  echo "重新连接到qwen2会话执行：tmux attach-session -t qwen2"
+fi
+if [ $(ps -ef | grep '[o]llama run llama3.1:8b' | grep -v grep | wc -l) -eq 0 ]; then
+  tmux new-session -d -s llama3 'bash -c "ollama run llama3.1:8b; exec bash"'
+  echo "连接llama3:tmux attach-session -t llama3"
+fi
