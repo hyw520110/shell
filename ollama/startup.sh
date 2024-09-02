78aae94d37eb5b4f5e53ddbb79165c10f567b979
hyw520110 <hyw520110@163.com>
2024-09-02 03:14:59 +0000

    Update startup.sh

--- ollama/startup.sh
+++ ollama/startup.sh
@@ -18,13 +18,15 @@ fi
 
 if [ $(ps -ef | grep 'ollama serve' |grep -v grep| wc -l) -eq 0 ]; then
   nohup ollama serve > ollama.log 2>&1 &
+  sleep 5
   #ollama pull nomic-embed-text
 fi
 if [ $(ollama list|grep qwen2|wc -l) -eq 0 ];then
   ollama pull qwen2:7b
 fi
 if [ $(ollama list|grep llama3|wc -l) -eq 0 ];then
-  ollama pull llama3.1:8b
+  ollama pull llama3.1:8b 2>/dev/null
+  support=$?
 fi
 ollama list
 if [ $(ps -ef | grep 'ollama run qwen2:7b' | grep -v grep | wc -l) -eq 0 ]; then
@@ -32,8 +34,7 @@ if [ $(ps -ef | grep 'ollama run qwen2:7b' | grep -v grep | wc -l) -eq 0 ]; then
   tmux new-session -d -s qwen2 'bash -c "ollama run qwen2:7b; exec bash"'
   echo "重新连接到qwen2会话执行：tmux attach-session -t qwen2"
 fi
-if [ $(ps -ef | grep 'ollama run llama3.1:8b' | grep -v grep | wc -l) -eq 0 ]; then
+if [ $support -eq 0 ] && [ $(ps -ef | grep 'ollama run llama3.1:8b' | grep -v grep | wc -l) -eq 0 ]; then
   tmux new-session -d -s llama3 'bash -c "ollama run llama3.1:8b; exec bash"'
   echo "连接llama3:tmux attach-session -t llama3"
 fi
-
